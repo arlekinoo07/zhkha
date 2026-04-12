424824abc21cd8d5f5c80b4323a56ba1d5c8dcc3
arlekinoo07 <ekim_alex07@mail.ru>
2026-04-12 16:58:45 +0300

    Update first block layou

--- app/main/block2/index.tsx
+++ app/main/block2/index.tsx
@@ -1,7 +1,6 @@
 export default function Block2() {
     return(
-        <>
-            <div className="pt-[780px] bg-gray-200">
+        <section className="bg-gray-200 py-[48px]">
                 <div className="flex justify-between px-[120px]">
                     <div className="bg-white w-fit pl-[24px] pr-[100px] py-[12px] rounded-xl shadow-2xl">
                         <p className="font-bold text-[20px] text-blue-500">Всего обращений: <span className="text-black">128</span></p>
@@ -13,7 +12,6 @@ export default function Block2() {
                         <p className="font-bold text-[20px] text-blue-500">Выполнено: <span className="text-black">128</span></p>
                     </div>
                 </div>
-            </div>
-        </>
+        </section>
     );
 }
